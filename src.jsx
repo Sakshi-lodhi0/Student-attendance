d0594bde2d394d2fa0be9f3e8b5a9d279deb038d
SAKSHI LODHI <149760694+Sakshi-lodhi0@users.noreply.github.com>
2024-01-04 16:08:36 +0530

    Add files via upload

---
+++ src.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to My Portfolio - Home</h2>
+      {/* Add your personal information here */}
+    </div>
+  );
+};
+
+export default Home;
